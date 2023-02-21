e24ab347113f492d17c3bb2efe7f0575a1d0e529
Uildes da Costa Demétrio <uildesdemetrio@inteligir.com.br>
2023-02-21 18:00:08 -0300

    first: configurating dependencies and files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import egetLogo from './assets/images/logo-eget.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={egetLogo} alt="Logo e-Get" />
+    </div>
+  )
+}
+
+export default App
